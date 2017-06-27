085133e119c2a59d3a0ef1193f18749abad76f97
EmmAlva <clioemma_28@hotmail.com>
2017-06-27 14:08:11 -0500

    js ready

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,11 +2,8 @@
 
 const state = {
 	datos: null,
-	name: null,
-	lastname: null
+	
 }
-
-
 
 $.getJSON('http://laboratoria.cuadra.co:9339/api/v1/students/',function(data,status,xhr){
 	console.log(data);
@@ -15,37 +12,16 @@ $.getJSON('http://laboratoria.cuadra.co:9339/api/v1/students/',function(data,sta
 	state.datos = data;
 	console.log(state.datos);
 
-
-//UBICACIONES
-	var array = [];
-	var info = "";
-	$.each(state.datos, function (index, ele){
-
-		info = ele.name;		
-
-		/*$('ol').append($('li').text(info));*/
-		array.push(info);
+	$.each(data, function(indice,elemento){
+		console.log(elemento);
+		let li = $('<li>'+ elemento.name+ '</li>');
+		let check = $('<input type="checkbox">');
+		//$('li').text(elemento.name);
+		li.append(check);
+		$('.asistencia').append(li);
 		
-	});
-	console.log(array);
-
-	for (var i in array){
-		/*$('ol').append($('li').text(info));*/
-
-	}
-
-	$.each(state.datos, function(indice,elemento){
-		var li = $('<li></li>');
-		"El indice"+indice+"contiene"+$('ol').text(elemento.name);
-
-		console.log("El indice"+indice+"contiene"+$('li').text(elemento.name));
-
-
-		
+		//console.log("El indice"+indice+"contiene"+$('li').html(elemento.name));
+		//console.log(elemento.name);		
 	})
 	
-	
-
-	});
-
-
+});
